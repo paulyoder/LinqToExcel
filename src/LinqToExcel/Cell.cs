7f3b8d19f52a0cc70ad6517539fd9c74e577e13e
Paul Yoder <paulyoder@gmail.com>
2009-01-12 16:02:56 +0000

    * Added Row and Cell classes * Renamed ExcelRepository.cs to ExcelRepositoryGeneric.cs * Renamed IExcelRepository.cs to IExcelRepositoryGeneric.cs
    git-svn-id: http://linqtoexcel.googlecode.com/svn/trunk@20 eb8a1d2c-adb8-11dd-8e2c-133076370c3f

---
+++ src/LinqToExcel/Cell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqToExcel
+{
+    /// <summary>
+    /// Represents a cell and its value in an excel spreadsheet
+    /// </summary>
+    public class Cell
+    {
+        /// <summary>
+        /// Cell's value
+        /// </summary>
+        public object Value { get; private set; }
+
+        /// <param name="value">Cell's value</param>
+        public Cell(object value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Returns the cell's value converted as the generic argument type
+        /// </summary>
+        /// <typeparam name="T">Object type to convert to</typeparam>
+        public T ValueAs<T>()
+        {
+            return (Value == null || Value is DBNull) ?
+                default(T) :
+                (T)Convert.ChangeType(Value, typeof(T));
+        }
+    }
+}
---
+++ src/LinqToExcel/Row.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqToExcel
+{
+    public class Row : List<Cell>
+    {
+        IDictionary<string, int> _columnIndexMapping;
+
+        /// <param name="cells">List of cells contained within the row</param>
+        /// <param name="columnIndexMapping">Column name to cell index mapping</param>
+        public Row(IList<Cell> cells, IDictionary<string, int> columnIndexMapping)
+        {
+            for (int i = 0; i < cells.Count; i++)
+                this.Insert(i, cells[i]);
+            _columnIndexMapping = columnIndexMapping;
+        }
+
+        /// <param name="columnName">Column Name</param>
+        public Cell this[string columnName]
+        {
+            get 
+            {
+                if (!_columnIndexMapping.ContainsKey(columnName))
+                    throw new ArgumentException(string.Format("Column name does not exist: {0}", columnName));
+                return base[_columnIndexMapping[columnName]]; 
+            }
+        }
+    }
+}
