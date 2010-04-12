c767cccc4ef02ac7a6305b4c6154c75d73e6d3dc
Paul Yoder <paulyoder@gmail.com>
2010-04-12 13:15:33 +0000

    * Forgot to add RowNoHeader to project
    git-svn-id: http://linqtoexcel.googlecode.com/svn/trunk@63 eb8a1d2c-adb8-11dd-8e2c-133076370c3f

---
+++ src/LinqToExcel/Domain/RowNoHeader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqToExcel
+{
+    public class RowNoHeader : List<Cell>
+    {
+        /// <param name="cells">Cells contained within the row</param>
+        public RowNoHeader(IEnumerable<Cell> cells)
+        {
+            base.AddRange(cells);
+        }
+    }
+}
