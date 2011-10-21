a996e51a8d72059e4435081c9cf16d90d3f19914
Paul Yoder <paulyoder@gmail.com>
2011-10-21 08:39:14 -0500

    LinqToExcel now works with the client profile framework versions. Thanks goes to Michael Keymolen for providing the fix

--- src/LinqToExcel/Properties/AssemblyInfo.cs
+++ src/LinqToExcel/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LinqToExcel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Easily retrieve data from spreadsheets and csv files by using LINQ")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("LinqToExcel")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2008")]
+[assembly: AssemblyCopyright("Copyright © Paul Yoder 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.2.0")]
+[assembly: AssemblyFileVersion("1.5.2.0")]
