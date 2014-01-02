ec5bafbff13712a6c7216b7f133addb490c28b1b
Jason Senich <jsenich@newsignature.com>
2014-01-01 20:16:22 -0500

    Add test for column mapping using annotations.

---
+++ src/LinqToExcel.Tests/CompanyWithColumnAnnotations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LinqToExcel.Attributes;
+
+namespace LinqToExcel.Tests
+{
+    public class CompanyWithColumnAnnotations
+    {
+        [ExcelColumn("Company Title")]
+        public string Name { get; set; }
+
+        [ExcelColumn("Boss")]
+        public string CEO { get; set; }
+        
+        [ExcelColumn("Number of People")]
+        public int EmployeeCount { get; set; }
+
+        [ExcelColumn("Initiation Date")]
+        public DateTime StartDate { get; set; }
+
+        [ExcelColumn("Active")]
+        public string IsActive { get; set; }
+    }
+}
