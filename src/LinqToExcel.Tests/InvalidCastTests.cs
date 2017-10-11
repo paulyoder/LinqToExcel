86943c69aaae6bd7a6b78963691790468fe9efd7
Alberto Chvaicer <alberto.chvaicer@leste.com>
2017-10-11 15:55:55 -0300

    Throw Exception with row number and column name/number

---
+++ src/LinqToExcel.Tests/InvalidCastTests.cs
@@ -0,0 +1,53 @@
+﻿using MbUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LinqToExcel.Tests
+{
+    [Author("Alberto Chvaicer", "achvaicer@gmail.com")]
+    [FixtureCategory("Integration")]
+    [TestFixture]
+    public class InvalidCastTests
+    {
+        private IExcelQueryFactory _factory;
+        private string _excelFileName;
+
+        [TestFixtureSetUp]
+        public void fs()
+        {
+            var testDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var excelFilesDirectory = Path.Combine(testDirectory, "ExcelFiles");
+            _excelFileName = Path.Combine(excelFilesDirectory, "Companies.xls");
+        }
+
+        [SetUp]
+        public void s()
+        {
+            _factory = new ExcelQueryFactory(_excelFileName, new LogManagerFactory());
+        }
+
+        [Test]
+        [ExpectedException(typeof(LinqToExcel.Exceptions.ExcelException), "Error on row 8 and column name 'EmployeeCount'.")]
+        public void invalid_number_cast_with_header()
+        {
+            var list = (from x in ExcelQueryFactory.Worksheet<Company>("Invalid Cast", _excelFileName, new LogManagerFactory())
+                        select x).ToList();
+
+        }
+
+        [Test, Ignore]
+        [ExpectedException(typeof(LinqToExcel.Exceptions.NoHeaderExcelException), "Error on row 8 and column 3.")]
+        public void invalid_number_cast_without_header()
+        {
+            
+            var list = (from x in _factory.WorksheetRangeNoHeader("A2", "D9", "Invalid Cast")
+                        where x[2].Cast<double>() > 30.0
+                        select x[2]).ToList();
+        }
+
+
+    }
+}
---
+++ src/LinqToExcel/Exceptions/ExcelException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqToExcel.Exceptions
+{
+    public class ExcelException : Exception
+    {
+        /// <summary>
+        /// Row index where exception occours
+        /// </summary>
+        public int Row { get; set; }
+
+        /// <summary>
+        /// Column name where exception occours
+        /// </summary>
+        public string ColumnName { get; set; }
+
+        
+        public ExcelException(int row, string columnName, Exception innerException)
+            : base(string.Format("Error on row {0} and column name '{1}'.", row, columnName), innerException)
+        {
+            Row = row;
+            ColumnName = columnName;
+        }
+    }
+}
