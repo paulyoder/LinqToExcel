eb707987408c3970e058048aded6c5b7de61e171
Stephen Workman <workman.stephen@gmail.com>
2017-10-21 19:17:05 -0400

    Bump assembly to version 2.0.0.

--- src/LinqToExcel/Properties/AssemblyInfo.cs
+++ src/LinqToExcel/Properties/AssemblyInfo.cs
@@ -31,7 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.11.0")]
-[assembly: AssemblyFileVersion("1.11.0")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-PRERELEASE")]
 
 [assembly: System.Security.AllowPartiallyTrustedCallers]
